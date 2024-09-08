6a0c318ac68c7ce751d51c0d202e5126bfc8430f
Dmitriy Parshin <parshyn.dima@gmail.com>
2024-09-08 17:07:01 +0300

    First commit

---
+++ dictionaries_hw2.py
@@ -0,0 +1,17 @@
+"""
+    Проверьте, есть ли в словаре ключ country
+    Выведите значение по-умолчанию "Россия" для ключа country
+    Добавьте в словарь элемент date со значением "27.05.2019"
+    Выведите на экран длину словаря
+"""
+
+weather = {
+    "city": "Москва",
+    "temperature": "20"
+}
+
+print("country" in weather)
+print(weather.setdefault("country", "Россия"))
+weather['date'] = '27.05.2019'
+print(weather)
+print(len(weather))
